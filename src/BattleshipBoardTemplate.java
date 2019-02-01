0c2be474fb83d80d8b0e9761f58c889f68a9e5ae
Jesse <jhan3537@bths.edu>
2019-02-01 10:12:28 -0500

    added base of gameplay field

--- src/BattleshipBoardTemplate.java
+++ src/BattleshipBoardTemplate.java
@@ -1,5 +1,12 @@
 public abstract class BattleshipBoardTemplate {
-    private Space[][] board;
+    private Space[][] board = new Space[10][10];
+    public BattleshipBoardTemplate() {
+        for (Space[] i : board) {
+            for (Space j : i) {
+                j = new Space();
+            }
+        }
+    }
 
     public abstract void selectSpace();
     public abstract void hit();
