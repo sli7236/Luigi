6f47154290cb85e8712ec0c7e865ab78e4c55144
BT_1N3_18 <f>
2019-02-01 11:42:03 -0500

    f

--- src/GameBattleShip.java
+++ src/GameBattleShip.java
@@ -1,6 +1,23 @@
+import java.util.*;
+
 public class GameBattleShip {
-    PlayerBoard playerBoard = new PlayerBoard(10, 10);
-    CPUBoard cpuBoard = new CPUBoard(10, 10);
+    private PlayerBoard playerBoard = new PlayerBoard(10, 10);
+    private CPUBoard cpuBoard = new CPUBoard(10, 10);
+    private int playerSelectedSpaces = 0;
+    private Space selectedSpace;
+    private Space secondSpace;
+
+    public void placeDownShip(int lengthOfShip, int row, int column) {
+        if (playerSelectedSpaces == 0) {
+            playerBoard.selectSpace(row, column);
+            selectedSpace = playerBoard.returnSpace(row, column);
+            playerSelectedSpaces ++;
+        }
+        else if (playerSelectedSpaces == 1) {
+            playerBoard.selectSpace(row, column);
+            secondSpace = playerBoard.returnSpace(row, column);
+        }
 
 
+    }
 }
