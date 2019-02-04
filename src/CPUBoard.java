ebf43089e8ddc9ed3caa027a31b636cf68c3c04f
BT_1N3_19 <cchau38462bths.edu>
2019-02-04 11:24:33 -0500

    compShip

--- src/CPUBoard.java
+++ src/CPUBoard.java
@@ -10,6 +10,21 @@ public class CPUBoard extends BattleshipBoardTemplate {
         }
     }
 
+    public void compShip(){
+        //ship space 5, space 4, space 3, space 2, space 2
+        int shipNum = 1; //shipNum 1 is the largest ship with 5 spaces
+        for (int i = shipNum; i < 6; i++) {
+            int randRow = (int) (Math.random() * 10);
+            int randCol = (int) (Math.random() * 10);
+            int direct = (int) (Math.random() * 2); //determines the direction of the ship that is being placed-- 1 is horizontal and 2 is vertical
+            if (shipNum == 1){
+                if(direct == 1){
+
+                }
+            }
+        }
+    }
+
     public boolean selectSpace(int row, int column) { // selecting a cpu board will shoot it
         return hit(row, column);
     }
