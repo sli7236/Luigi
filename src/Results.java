5be05d36bbb3c6b53174529567125f9c5c8dfc76
BT_1N3_18 <sli7236@bths.edu>
2019-02-07 11:33:25 -0500

    jesus

--- src/Results.java
+++ src/Results.java
@@ -64,29 +64,6 @@ public class Results extends Application{
         stage.setScene(scene);
         stage.show();
 
-        /*final CategoryAxis xAxis = new CategoryAxis();
-        final NumberAxis yAxis = new NumberAxis();
-        xAxis.setLabel("School");
-
-        final LineChart<String,Number> lineChart =
-                new LineChart<String,Number>(xAxis,yAxis);
-
-        lineChart.setTitle("SAT Results 2012");
-
-        XYChart.Series series = new XYChart.Series();
-        series.setName("SAT Math");
-
-
-
-        for(int i = 0; i<data.size();i++){
-            Data   current = data.get(i);
-            series.getData().add(new XYChart.Data(current.getPlayerName(), current.getScore()));
-        }
-
-
-        Scene scene  = new Scene(lineChart,800,600);
-        lineChart.getData().add(series);*/
-
     }
 
     public static void main(String[] args) {
