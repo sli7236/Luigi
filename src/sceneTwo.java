d960d0cf932ff37b972e2337b8120902d406cb78
Christina Chau <christinachau5@gmail.com>
2019-02-08 00:07:49 -0500

    I CANT GET ANYTHING DONE BECAUSE INTELLIJ IS LAGGING ODE

---
+++ src/sceneTwo.java
@@ -0,0 +1,25 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import java.io.IOException;
+
+public class sceneTwo implements Initializable{
+    @FXML
+    private AnchorPane sceneTwo;
+
+    @Override
+    public void initialize(URL url,ResourceBundle rb){
+    }
+
+    @FXML
+    private void nextSceneThree(ActionEvent event) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("instructions.fxml"));
+        sceneTwo.getChildren().setAll(pane);
+    }
+
+}
