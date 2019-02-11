28f4ae5f9010c735bea33d4158c2ac7d969a1f14
Christina Chau <christinachau5@gmail.com>
2019-02-10 21:02:28 -0500

    how long does it take to finish this jfc

--- src/resultsFXML.java
+++ src/resultsFXML.java
@@ -1,19 +1,25 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
 import java.util.List;
 
 public class resultsFXML implements Initializable{
+    @FXML private AnchorPane scoreScene;
     @FXML private TableView<CSVReader.Data> tableView;
     @FXML private TableColumn<playInfo,String> nameColumn;
     @FXML private TableColumn<playInfo,Integer> scoreColumn;
@@ -22,7 +28,10 @@ public class resultsFXML implements Initializable{
     @FXML private TableColumn<playInfo,Integer> pHitColumn;
     @FXML private TableColumn<playInfo,Integer> pSunkColumn;
 
-    public resultsFXML() {
+    @FXML
+    public void backLoadScene(ActionEvent event) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("loadUp.fxml"));
+        scoreScene.getChildren().setAll(pane);
     }
 
     @Override
